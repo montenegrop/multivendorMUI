f4bfec58d9d31e04f8e4ea06410466e4156f54cd
Bruno Ramari <kbruskbrus@gmail.com>
2021-05-03 21:18:48 -0300

    ProductManager styling

--- src/products/views/ProductManager/ProductManager.tsx
+++ src/products/views/ProductManager/ProductManager.tsx
@@ -1,7 +1,19 @@
+import Container from "@saleor/macaw-ui/Container";
+import PageHeader from "@saleor/macaw-ui/PageHeader";
 import React from "react";
 
+import ProductCreateForm from "../../components/ProductCreatePage/form";
+
 const ProductManager: React.FC = props => (
-  <div>mira papu aca soy yo el que crea produtos</div>
+  <>
+    <Container>
+      <PageHeader title={"Agrega tus productos"} />
+      <div>mira papu aca soy yo el que crea produtos</div>
+      <ProductCreateForm>
+        {() => <input type="text" placeholder="hello papi" />}
+      </ProductCreateForm>
+    </Container>
+  </>
 );
 
 export default ProductManager;
