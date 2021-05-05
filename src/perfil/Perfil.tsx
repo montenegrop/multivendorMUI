3fe977354aa6cbe8a38b473ad04e0e2224fd4e9f
Bruno Ramari <kbruskbrus@gmail.com>
2021-05-05 10:02:34 -0300

    cambios de form user-data manejados

--- src/perfil/Perfil.tsx
+++ src/perfil/Perfil.tsx
@@ -28,7 +28,16 @@ const useStyles = makeStyles(
 );
 
 const Perfil: React.FC = props => {
-  const user = useUser();
+  const { user } = useUser();
+  const initialForm = {
+    email: user.email,
+    firstName: user.firstName,
+    id: user.id,
+    identification: user.identification,
+    lastName: user.lastName,
+    phone: user.phone,
+    typeOfIdentification: user.typeOfIdentification
+  };
   // console.log(user);
 
   const classes = useStyles(props);
@@ -36,80 +45,88 @@ const Perfil: React.FC = props => {
     <>
       <Container>
         <PageHeader title={"Datos de Perfil"} />
+        <Form initial={initialForm}>
+          {({ change, data }) => (
+            <>
+              <Card id="user-data">
+                <CardTitle title={"Tus datos de Usuario"} />
 
-        <Card>
-          <CardTitle title={"Tus datos de Usuario"} />
-
-          <CardContent>
-            <div className={classes.root}>
-              <TextField
-                disabled={false}
-                error={false}
-                name="firstName"
-                label="Nombre"
-                value={user.user.firstName}
-                onChange={() => null}
-              />
-              <TextField
-                disabled={false}
-                error={false}
-                name="lastName"
-                label="Apellido"
-                value={user.user.lastName}
-                onChange={() => null}
-              />
-              <TextField
-                disabled={false}
-                error={false}
-                name="email"
-                label="Email"
-                value={user.user.email}
-                onChange={() => null}
-              />
-              <TextField
-                disabled={false}
-                error={false}
-                name="phone"
-                label="Telefono"
-                value={user.user.phone}
-                onChange={() => null}
-              />
-              <TextField
-                disabled={false}
-                error={false}
-                name="identification"
-                label="Número de Identificacion"
-                value={user.user.identification}
-                onChange={() => null}
-              />
-              <div>
-                <InputLabel id="demo-simple-select-label">Tipo</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={user.user.typeOfIdentification}
-                  onChange={() => null}
-                  fullWidth
-                >
-                  <MenuItem value={"dni"}>DNI</MenuItem>
-                  <MenuItem value={"passport"}>PASAPORTE</MenuItem>
-                </Select>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardTitle title={"Lo que va a ver tu Cliente"} />
-          <CardContent>
-            <div className={classes.root}></div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardTitle title={"Los servicios que ofreces"} />
-          <CardContent>
-            <div className={classes.root}></div>
-          </CardContent>
-        </Card>
+                <CardContent>
+                  <div className={classes.root}>
+                    <TextField
+                      disabled={false}
+                      error={false}
+                      name="firstName"
+                      label="Nombre"
+                      value={data.firstName}
+                      onChange={change}
+                    />
+                    <TextField
+                      disabled={false}
+                      error={false}
+                      name="lastName"
+                      label="Apellido"
+                      value={data.lastName}
+                      onChange={change}
+                    />
+                    <TextField
+                      disabled={false}
+                      error={false}
+                      name="email"
+                      label="Email"
+                      value={data.email}
+                      onChange={change}
+                    />
+                    <TextField
+                      disabled={false}
+                      error={false}
+                      name="phone"
+                      label="Telefono"
+                      value={data.phone}
+                      onChange={change}
+                    />
+                    <TextField
+                      disabled={false}
+                      error={false}
+                      name="identification"
+                      label="Número de Identificacion"
+                      value={data.identification}
+                      onChange={change}
+                    />
+                    <div>
+                      <InputLabel id="demo-simple-select-label">
+                        Tipo
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={data.typeOfIdentification}
+                        name="typeOfIdentification"
+                        onChange={change}
+                        fullWidth
+                      >
+                        <MenuItem value={"dni"}>DNI</MenuItem>
+                        <MenuItem value={"passport"}>PASAPORTE</MenuItem>
+                      </Select>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+              <Card id="vendor-data">
+                <CardTitle title={"Lo que va a ver tu Cliente"} />
+                <CardContent>
+                  <div className={classes.root}></div>
+                </CardContent>
+              </Card>
+              <Card id="services-data">
+                <CardTitle title={"Los servicios que ofreces"} />
+                <CardContent>
+                  <div className={classes.root}></div>
+                </CardContent>
+              </Card>
+            </>
+          )}
+        </Form>
       </Container>
     </>
   );
