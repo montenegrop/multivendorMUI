4756635a14b8630123afc2eec6aa61cc0af13df1
Bruno Ramari <kbruskbrus@gmail.com>
2021-05-10 13:35:55 -0300

    Location API trae provincias, ciudades

--- src/perfil/Perfil.tsx
+++ src/perfil/Perfil.tsx
@@ -10,7 +10,7 @@ import { CardSpacer } from "@saleor/components/CardSpacer";
 import CardTitle from "@saleor/components/CardTitle";
 import { SaveButtonBar } from "@saleor/components/SaveButtonBar";
 import useUser from "@saleor/hooks/useUser";
-import React from "react";
+import React, { useEffect } from "react";
 import Dropzone from "react-dropzone/dist/index";
 
 import Container from "../components/Container";
@@ -31,6 +31,16 @@ const useStyles = makeStyles(
 );
 const useStylesVendor = makeStyles(
   theme => ({
+    datePicker: {
+      margin: "auto 0",
+      width: "36%"
+    },
+    datePickerContainer: {
+      display: "grid",
+      gridColumnEnd: "3",
+      gridColumnStart: "1",
+      gridTemplateColumns: "1fr"
+    },
     dragActive: { background: "rgba(200,200,200,0.2)", cursor: "pointer" },
     dropContainer: {
       gridColumnEnd: "3",
@@ -56,6 +66,13 @@ const useStylesVendor = makeStyles(
     label: {
       marginBottom: "1rem"
     },
+    location: {
+      columnGap: "1rem",
+      display: "grid",
+      gridColumnEnd: "3",
+      gridColumnStart: "1",
+      gridTemplateColumns: "3fr 3fr 2fr"
+    },
     root: {
       display: "grid",
       gridColumnGap: theme.spacing(2),
@@ -64,6 +81,8 @@ const useStylesVendor = makeStyles(
       gridTemplateRows: "260px 1fr "
     },
     textarea: {
+      gridColumnEnd: "3",
+      gridColumnStart: "1",
       height: "100%",
       margin: "1rem auto",
       width: "100%"
@@ -91,6 +110,12 @@ const Perfil: React.FC = props => {
     id: user.id,
     identification: user.identification,
     lastName: user.lastName,
+    city: (perfilVendorData && perfilVendorData.location?.city) || "undefined",
+    postalCode:
+      (perfilVendorData && perfilVendorData.location?.postalCode) ||
+      "undefined",
+    province:
+      (perfilVendorData && perfilVendorData.location?.province) || "undefined",
     phone: user.phone,
     typeOfIdentification: user.typeOfIdentification
   };
@@ -109,7 +134,43 @@ const Perfil: React.FC = props => {
     // console.log(data);
   };
 
-  const loading = false;
+  const loading = false; // Aca va el estado loading de la mutation cuando esta guardando
+
+  // lista de ciudades provincias
+
+  const [provincias, setProvincias] = React.useState([]);
+  const [ciudades, setCiudades] = React.useState([]);
+
+  useEffect(() => {
+    fetch("https://apis.datos.gob.ar/georef/api/provincias?")
+      .then(response => response.json())
+      .then(data =>
+        setProvincias(data.provincias.map(provincia => provincia.nombre))
+      );
+  }, []);
+
+  const getCities = provincia => {
+    provincia = provincia
+      .split(" ")
+      .join("-")
+      .toLowerCase();
+
+    fetch(
+      `https://apis.datos.gob.ar/georef/api/localidades?provincia=${provincia}&max=1500`
+    )
+      .then(response => response.json())
+      .then(data =>
+        setCiudades(
+          Array.from(
+            new Set(
+              data.localidades
+                .map(localidad => localidad.localidad_censal.nombre)
+                .sort()
+            )
+          )
+        )
+      );
+  };
 
   return (
     <>
@@ -219,6 +280,91 @@ const Perfil: React.FC = props => {
                         El tamaño recomendado es de 970px x 250px
                       </div>
                     </div>
+                    <div
+                      id="datePicker"
+                      className={classesVendor.datePickerContainer}
+                    >
+                      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <DatePicker
+                          className={classesVendor.datePicker}
+                          id="foundingYearInput"
+                          inputVariant="standard"
+                          label="¿En qué año empezaste a Trabajar?"
+                          name="foundingYear"
+                          autoOk
+                          variant="dialog"
+                          views={["year"]}
+                          value={data.foundingYear}
+                          maxDate={new Date()}
+                          onChange={date => {
+                            change({
+                              target: {
+                                name: "foundingYear",
+                                value: new Date(date.getFullYear(), 0, 1)
+                              }
+                            });
+                          }}
+                          InputLabelProps={{
+                            shrink: true
+                          }}
+                        />
+                      </MuiPickersUtilsProvider>
+                    </div>
+                    <div id="ubicacion" className={classesVendor.location}>
+                      <div>
+                        <InputLabel id="provinceLabel">Provincia</InputLabel>
+                        <TextField
+                          select
+                          id="provincia"
+                          value={data.province}
+                          name="province"
+                          onChange={e => {
+                            change(e);
+                            getCities(e.target.value);
+                            return;
+                          }}
+                          variant="standard"
+                          fullWidth
+                        >
+                          {provincias.map((provincia, indx) => (
+                            <MenuItem key={indx} value={provincia}>
+                              {provincia}
+                            </MenuItem>
+                          ))}
+                          <MenuItem value={"undefined"} disabled></MenuItem>
+                        </TextField>
+                      </div>
+                      <div>
+                        <InputLabel id="cityLabel">Ciudad</InputLabel>
+                        <TextField
+                          select
+                          id="city"
+                          value={data.city}
+                          name="city"
+                          onChange={change}
+                          variant="standard"
+                          fullWidth
+                        >
+                          {ciudades.map((ciudad, indx) => (
+                            <MenuItem key={indx} value={ciudad}>
+                              {ciudad}
+                            </MenuItem>
+                          ))}
+                          <MenuItem value={"undefined"} disabled></MenuItem>
+                        </TextField>
+                      </div>
+                      <div>
+                        <InputLabel id="postalCodeLabel">C.P.:</InputLabel>
+                        <TextField
+                          id="postalCode"
+                          value={data.postalCode}
+                          name="postalCode"
+                          onChange={change}
+                          variant="standard"
+                          fullWidth
+                        />
+                      </div>
+                    </div>
                     <TextField
                       id="descripcion"
                       label="Descripcion"
@@ -227,31 +373,6 @@ const Perfil: React.FC = props => {
                       multiline
                       className={classesVendor.textarea}
                     />
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                      <DatePicker
-                        id="foundingYearInput"
-                        inputVariant="standard"
-                        label="Fecha de Inicio de Actividades"
-                        name="foundingYear"
-                        autoOk
-                        variant="dialog"
-                        views={["year"]}
-                        value={data.foundingYear}
-                        maxDate={new Date()}
-                        helperText="¿Cuando empezaste a trabajar?"
-                        onChange={date => {
-                          change({
-                            target: {
-                              name: "foundingYear",
-                              value: new Date(date.getFullYear(), 0, 1)
-                            }
-                          });
-                        }}
-                        InputLabelProps={{
-                          shrink: true
-                        }}
-                      />
-                    </MuiPickersUtilsProvider>
                   </div>
                 </CardContent>
               </Card>
