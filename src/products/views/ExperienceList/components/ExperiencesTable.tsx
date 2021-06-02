45098145a1add2ceca0930a20dac93b57f0ead40
Bruno Ramari <kbruskbrus@gmail.com>
2021-06-02 13:45:51 -0300

    Tabla con Saleor Components

--- src/products/views/ExperienceList/components/ExperiencesTable.tsx
+++ src/products/views/ExperienceList/components/ExperiencesTable.tsx
@@ -1,49 +1,75 @@
 import { Card, CardContent } from "@material-ui/core";
-import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
+import TableFooter from "@material-ui/core/TableFooter";
 import TableRow from "@material-ui/core/TableRow";
+import Checkbox from "@saleor/components/Checkbox";
+import ResponsiveTable from "@saleor/components/ResponsiveTable";
+import Skeleton from "@saleor/components/Skeleton";
+import TableCellHeader from "@saleor/components/TableCellHeader";
+import TableHead from "@saleor/components/TableHead";
 import React from "react";
+
+const useStyles = makeStyles(
+  theme => ({
+    colImages: {
+      width: "15%"
+    },
+    imgSize: {
+      height: "auto",
+      maxHeight: "60px",
+      maxWidth: "110px",
+      objectFit: "contain",
+      width: "100%"
+    }
+  }),
+  { name: "ExperiencesTable" }
+);
 
 const headers = ["Imagen Principal", "Descripción Corta"];
 
 const ExperiencesTable = props => {
   const { data } = props;
   const experiences = data.filter(dat => dat.descriptionShort !== "");
+  const classes = useStyles({ props });
   // console.log(experiences);
   return (
     <Card>
       <CardContent>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                {headers.map(label => (
-                  <TableCell key={label}>{label}</TableCell>
-                ))}
+        <ResponsiveTable>
+          <TableHead
+            selected={0}
+            colSpan={3}
+            disabled={true}
+            items={experiences}
+          >
+            {headers.map(head => (
+              <TableCellHeader
+                key={head}
+                className={`${head === headers[0] ? classes.colImages : null}`}
+              >
+                {head}
+              </TableCellHeader>
+            ))}
+          </TableHead>
+          <TableBody>
+            {experiences.map(exp => (
+              <TableRow key={exp.descriptionShort}>
+                <TableCell padding="checkbox">
+                  <Checkbox disableClickPropagation></Checkbox>
+                </TableCell>
+                <TableCell>
+                  <img
+                    className={classes.imgSize}
+                    src={exp.pastExperienceImages.edges[0]?.node.url}
+                  />
+                </TableCell>
+                <TableCell>{exp.descriptionShort}</TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {experiences.map(exp => (
-                <TableRow>
-                  <TableCell>
-                    <img
-                      width="50"
-                      height="50"
-                      src={exp.pastExperienceImages.edges[0]?.node.url}
-                    />
-                  </TableCell>
-                  <TableCell>{exp.descriptionShort}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+            ))}
+          </TableBody>
+        </ResponsiveTable>
       </CardContent>
     </Card>
   );
