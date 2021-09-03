da97b03a0ff1d4f97e7bc2f0c8b3e5bf253ebeb2
montenegrop <pablormontenegro1@gmail.com>
2021-09-03 07:45:01 -0300

    table y query

--- src/services/views/ActiveServices.tsx
+++ src/services/views/ActiveServices.tsx
@@ -1,5 +1,27 @@
+import { CircularProgress, Container } from "@material-ui/core";
 import React from "react";
 
-const ActiveServices: React.FC = () => <div>hola</div>;
+import ActiveServicesPage from "../../services/components/ActiveServicesPage";
+import { ActiveServicesListQuery } from "./queries";
+
+const ActiveServices: React.FC = () => (
+  <ActiveServicesListQuery variables={{ vendorId: "uservendorId" }}>
+    {({ data, loading }) => {
+      if (loading) {
+        return (
+          <Container>
+            <CircularProgress />
+          </Container>
+        );
+      }
+
+      return (
+        <Container>
+          <ActiveServicesPage data="data de la query" />
+        </Container>
+      );
+    }}
+  </ActiveServicesListQuery>
+);
 
 export default ActiveServices;
