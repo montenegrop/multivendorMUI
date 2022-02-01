260e2d022a50de90bf8de656e631122f4c9e0975
montenegrop <pablo.rpm@gmail.com>
2022-01-31 21:13:04 -0300

    skuchange

--- constants.ts
+++ constants.ts
@@ -1,2 +1,7 @@
 export const newBaseProductTypeId = "UHJvZHVjdFR5cGU6MTg=";
 export const defaultProductTypeId = "UHJvZHVjdFR5cGU6MTI=";
+
+// localhost:
+
+// export const newBaseProductTypeId = "QmFzZVByb2R1Y3Q6MQ==";
+// export const defaultProductTypeId = "UHJvZHVjdFR5cGU6MQ==";
